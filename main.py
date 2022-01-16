65b3773198654af288b2f6d619c26def25f82311
RSMP <rubshan.3121@gmail.com>
2022-01-16 14:13:36 +0530

    16-01-2022

--- main.py
+++ main.py
@@ -1063,7 +1063,7 @@ def data_train():
     # img = cv2.imread(path)
     # aa = ['Acne and Rosacea Photos', 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
     #       'Atopic Dermatitis Photos', 'Bullous Disease Photos', 'Cellulitis Impetigo and other Bacterial Infections', 'Eczema Photos', 'Exanthems and Drug Eruptions', 'Herpes HPV and other STDs Photos', 'Light Diseases and Disorders of Pigmentation', 'Lupus and other Connective Tissue diseases', 'Melanoma Skin Cancer Nevi and Moles', 'Poison Ivy Photos and other Contact Dermatitis', 'Psoriasis pictures Lichen Planus and related diseases', 'Scabies Lyme Disease and other Infestations and Bites', 'Seborrheic Keratoses and other Benign Tumors', 'Systemic Disease', 'Tinea Ringworm Candidiasis and other Fungal Infections', 'Urticaria Hives', 'Vascular Tumors', 'Vasculitis Photos', 'Warts Molluscum and other Viral Infections']
-    aa = ['athelets_foot', 'atopic dermatisis on hand', 'chicken_pox',
+    aa = ['athelets_foot', 'atopic dermatisis on hand', 'black mole', 'chicken_pox',
           'cracked_heels', 'facial_acne']
     for i in aa:
         print(i)
